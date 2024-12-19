6c5dbc4c9e4e38d0b7c15667ec8bb5212db3ef00
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2024-12-20 02:45:10 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,13 @@
 # Download the logstash tar.gz file
 wget https://prod1-us.blusapphire.net/export/install/logstash-8.9.0.tar.gz -P /opt/snb-tech/Logdetector/
 
+# Assuming nexus.tar.gz is already present in the directory
 # Unzip the tar files
-unzip /opt/snb-tech/Logdetector/nexus.tar.gz -d /opt/snb-tech/Logdetector/
-unzip /opt/snb-tech/Logdetector/logstash-8.9.0.tar.gz -d /opt/snb-tech/Logdetector/
+tar -zxvf /opt/snb-tech/Logdetector/nexus.tar.gz -C /opt/snb-tech/Logdetector/
+tar -zxvf /opt/snb-tech/Logdetector/logstash-8.9.0.tar.gz -C /opt/snb-tech/Logdetector/
 
 # Copy the snb_windows.service file to the systemd directory
+# Make sure snb_windows.service is present in the current directory
 cp -r snb_windows.service /etc/systemd/system/
 
 # Create symbolic links for nexus and logstash
@@ -18,6 +20,5 @@ ln -s /opt/snb-tech/Logdetector/logstash-8.9.0 /opt/collector
 mkdir -p /opt/snb-tech/Logdetector/.install
 mv /opt/snb-tech/Logdetector/nexus.tar.gz /opt/snb-tech/Logdetector/logstash-8.9.0.tar.gz /opt/snb-tech/Logdetector/.install
 
-#End of script
-
-echo  #####  DONE  ######
+# End of script
+echo "##### DONE #####"
